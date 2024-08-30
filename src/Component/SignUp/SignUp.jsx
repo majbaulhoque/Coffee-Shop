bab43a5bf19b902703acebbf1097dadccadfdb79
Majba UL Hoque <majba9399@gmail.com>
2024-08-30 18:24:57 +0600

    create sign up page and user data store in firebase

--- src/Component/SignUp/SignUp.jsx
+++ src/Component/SignUp/SignUp.jsx
@@ -1,9 +1,59 @@
+import { useContext } from "react";
+import { AuthContext } from "../Provider/AuthProvider";
 
 
 const SignUp = () => {
+
+    const {createUser} = useContext(AuthContext);
+
+    const handleSignUp = e =>{
+        e.preventDefault();
+        const form = e.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+        createUser(email, password)
+        .then(res => {
+            console.log(res.user)
+        })
+        .catch(error => {
+            console.log(error.message)
+        })
+
+    }
+
     return (
         <div>
-            <h2>Please Sign Up </h2>
+            <div className="hero bg-base-200 min-h-screen">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <div className="text-center">
+                        <h1 className="text-5xl font-bold">Sign Up Now!</h1>
+                        
+                    </div>
+                    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                        <form onSubmit={handleSignUp} className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" placeholder="email" name="email" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" placeholder="password" name="password" className="input input-bordered" required />
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="btn btn-primary">Sign Up</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import Route from './Route/Route'
+import AuthProvider from './Component/Provider/AuthProvider'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={Route}></RouterProvider>
+    <AuthProvider>
+      <RouterProvider router={Route}></RouterProvider>
+    </AuthProvider>
   </StrictMode>,
 )
