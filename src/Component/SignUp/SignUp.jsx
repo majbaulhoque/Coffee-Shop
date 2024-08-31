2700f909a79cbe1b54716540e5b418417045b2bf
Majba UL Hoque <majba9399@gmail.com>
2024-08-31 20:30:01 +0600

    added delete button

--- src/Component/SignUp/SignUp.jsx
+++ src/Component/SignUp/SignUp.jsx
@@ -9,7 +9,7 @@ const SignUp = () => {
 
     const handleSignUp = async (e) => {
         e.preventDefault();
-        const form = e.target;
+        const form = e.target; 
         const email = form.email.value;
         const password = form.password.value;
         
--- src/Component/Users/Users.jsx
+++ src/Component/Users/Users.jsx
@@ -1,53 +1,101 @@
-import axios from "axios"
-import { useEffect, useState } from "react"
-import { BASE_URL } from "../../config"
-
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { BASE_URL } from "../../config";
+import { MdOutlineDelete } from "react-icons/md";
+import Swal from 'sweetalert2';
 
 const Users = () => {
-    const [userData, setUserData] = useState([])
+    const [userData, setUserData] = useState([]);
+
     const getUserData = async () => {
         try {
-            const res = await axios.get(`${BASE_URL}/user`)
-            setUserData(res.data)
-            console.log(res.data)
+            const res = await axios.get(`${BASE_URL}/user`);
+            setUserData(res.data);
+            console.log(res.data);
         } catch (error) {
-            console.log(error)
+            console.log(error);
         }
-    }
+    };
+
+    const handleDelete = async (id) => {
+        // Alert Confirmation
+        const result = await Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!',
+            cancelButtonText: 'Cancel'
+        });
+
+        if (result.isConfirmed) {
+            try {
+                const res = await axios.delete(`${BASE_URL}/user/${id}`);
+                console.log(res.data);
+                if (res.data.deletedCount > 0) {
+                    // Refresh the user list
+                    getUserData();
+                    Swal.fire(
+                        'Deleted!',
+                        'The user has been deleted.',
+                        'success'
+                    );
+                }
+            } catch (error) {
+                console.log(error);
+                Swal.fire(
+                    'Error!',
+                    'Something went wrong.',
+                    'error'
+                );
+            }
+        }
+    };
 
     useEffect(() => {
-        getUserData()
-    }, [])
+        getUserData();
+    }, []);
 
     return (
         <div>
-            <h2>Users : {userData.length}</h2>
+            <h2 className="text-center text-black text-4xl py-5 font-bold">Users : {userData.length}</h2>
             <div className="overflow-x-auto">
                 <table className="table">
                     {/* head */}
                     <thead>
-                        <tr>
-                            <th></th>
+                        <tr className="text-2xl text-black border-black">
+                            <th>SL.</th>
                             <th>Email</th>
                             <th>Created At</th>
                             <th>Action</th>
                         </tr>
                     </thead>
                     <tbody>
-                        {/* row 1 */}
+                        {/* rows */}
                         {
-                            userData?.map(user => <tr key={userData._id} className="border-black">
-                                <th>1</th>
-                                <td>{user.email}</td>
-                                <td>{user.createdAt}</td>
-                                <td>Blue</td>
-                            </tr>)
+                            userData?.map((user, idx) => (
+                                <tr key={user._id} className="border-black text-xl text-black ">
+                                    <th>{idx + 1}</th>
+                                    <td>{user.email}</td>
+                                    <td>{user.createdAt}</td>
+                                    <td>
+                                        <button 
+                                            onClick={() => handleDelete(user._id)} 
+                                            className="bg-red-600 h-8 w-8 flex justify-center items-center rounded-full"
+                                        >
+                                            <MdOutlineDelete />
+                                        </button>
+                                    </td>
+                                </tr>
+                            ))
                         }
                     </tbody>
                 </table>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Users
+export default Users;
